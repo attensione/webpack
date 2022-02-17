156dfe5506f79fec1838a6a735629acf522948f1
attensione <atrobz@gmail.com>
2022-02-17 13:32:41 +0100

    work

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,17 @@
 const PATH = require('path');
 
 module.export = {
-    mode: 'development',
     entry: PATH.resolve(__dirname, 'src'),
     output: {
         path: PATH.resolve(__dirname, 'dist'),
         filename: '[name].js',
     },
     module: {
-        rules: [
-            {
-                loader: 'ts-loader',   
-                test: /\.tsx?$/,
-                exclude: /node_modules/,            },
-        ]
+        rules: [{
+            loader: 'ts-loader',
+            test: /\.tsx?$/,
+            exclude: /node_modules/,
+        }, ]
     },
     resolve: {
         extensions: [".tsx", ".ts", ".js"]
