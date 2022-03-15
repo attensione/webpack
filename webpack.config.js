9e76c28e41dde1d52f45602a89f5627f3f12c959
aone.pl <atrobz@gmail.com>
2022-03-15 13:14:01 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,44 @@
 var glob = require('glob');
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
-    watch: true,
+    mode: 'development',
+    devtool: 'source-map',
+    watch: false,
     entry: {
-        //core: './src/core.ts', //one-file-loader
-        //core: ['./src/core.ts', './src/test.ts'], //files-from-table-loader
-        core: glob.sync('./src/**/*.ts*'), //all-files-from-folder-loader require var glob = require('glob');
+        core: glob.sync('./src/**/*.ts*'),
     },
     output: {
         filename: '[name].js',
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'assets/js'),
     },
     module: {
         rules: [{
-            test: /\.tsx?$/,
-            use: 'ts-loader',
-            exclude: /node_modules/,
-        }, ],
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.(s(a|c)ss)$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader', ],
+            },
+        ],
+    },
+    plugins: [new MiniCssExtractPlugin({
+        filename: '../css/[name].css',
+    })],
+    optimization: {
+        minimizer: [
+            new TerserPlugin({ parallel: true }),
+            new CssMinimizerPlugin(),
+        ],
+        minimize: true,
     },
     resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
+        extensions: ['*', '.tsx', '.ts', '.js'],
     },
     devServer: {
         static: {
